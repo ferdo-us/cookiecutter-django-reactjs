443859d2561d9f996c8b6054726e6d6db9c7bbf4
genomics-geek <gonzalezma@email.chop.edu>
2019-10-05 12:40:56 -0400

    Updated to using latest create-react-app

--- {{cookiecutter.project_slug}}/frontend/src/App.js
+++ {{cookiecutter.project_slug}}/frontend/src/App.js
@@ -24,11 +24,18 @@ class App extends Component {
         <DocumentTitle title="{{ cookiecutter.project_name }}" />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <button type="button" onClick={this.onClick}>
           Send API request
         </button>
